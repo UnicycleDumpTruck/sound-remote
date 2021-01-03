342cfc6e1ef15d601702252d2c6a5472bd988bcb
Jeff Highsmith <junk@jeffhighsmith.com>
2021-01-03 12:03:47 -0500

    Storing theme digit in text list.

--- main.ts
+++ main.ts
@@ -1,40 +1,43 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == current_sound && need_ack) {
+    if (receivedNumber == current_sound) {
         need_ack = false
     }
 })
 function send_ack_show () {
     need_ack = true
-    for (let index = 0; index < 4; index++) {
-        radio.sendNumber(current_sound + 60)
+    for (let index = 0; index < 3; index++) {
+        radio.sendNumber(current_sound)
         basic.pause(200)
         if (!(need_ack)) {
-            basic.showString("" + (text_list[0]))
+            basic.showString("" + (text_list[current_sound - theme_tens + 1]))
             break;
         }
     }
     current_sound = -1
 }
 input.onButtonPressed(Button.A, function () {
-    current_sound = randint(0, text_list.length - 1)
+    current_sound = randint(1, text_list.length - 1) + theme_tens
     send_ack_show()
 })
 input.onButtonPressed(Button.AB, function () {
-    current_sound = 1
+    current_sound = 1 + theme_tens
     send_ack_show()
 })
 input.onButtonPressed(Button.B, function () {
-    current_sound = 0
+    current_sound = 0 + theme_tens
     send_ack_show()
 })
+let theme_tens = 0
 let text_list: string[] = []
 let current_sound = 0
 let need_ack = false
 radio.setGroup(1)
 need_ack = false
 current_sound = -1
-// The Office
-text_list = ["BDay", "Pretzel"]
+// 60 The Office
+text_list = ["60", "BDay", "Pretzel"]
+theme_tens = parseFloat(text_list[0])
+basic.showNumber(theme_tens)
 basic.forever(function () {
 	
 })
